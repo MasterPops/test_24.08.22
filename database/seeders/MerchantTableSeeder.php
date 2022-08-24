64012ab754cfcdbe11696a403782d3dbc1ad5310
Alexey <masterpops.96@gmail.com>
2022-08-24 20:57:09 +0300

    migrations, seeder, models

---
+++ database/seeders/MerchantTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Merchant;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MerchantTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $merchant = new Merchant();
+      $merchant->external_id = 6;
+      $merchant->key = 'KaTf5tZYHx4v7pgZ';
+      $merchant->save();
+
+      $merchant = new Merchant();
+      $merchant->external_id = 816;
+      $merchant->key = 'rTaasVHeteGbhwBx';
+      $merchant->save();
+    }
+}
